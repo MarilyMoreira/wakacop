b70f77eabcf1d780308763fc8edaae782bfae6ff
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-10-30 16:04:12 -0300

    implementa controller. Teste Ok!

---
+++ src/main/java/academy/wakanda/wakacop/sessaovotacao/application/service/SessaoVotacaoApplicationService.java
@@ -0,0 +1,19 @@
+package academy.wakanda.wakacop.sessaovotacao.application.service;
+
+import academy.wakanda.wakacop.sessaovotacao.application.api.SessaoAberturaRequest;
+import academy.wakanda.wakacop.sessaovotacao.application.api.SessaoAberturaResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class SessaoVotacaoApplicationService implements SessaoVotacaoService {
+    @Override
+    public SessaoAberturaResponse abreSessao(SessaoAberturaRequest sessaoAberturaRequest) {
+        log.info("[start] SessaoVotacaoApplicationService - abreSessao");
+        log.info("[finish] SessaoVotacaoApplicationService - abreSessao");
+        return null;
+    }
+}
