1634401d695f4faf3b00f796a0b2377c9a601fbe
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-10-30 16:44:52 -0300

    implementa application service e infra

---
+++ src/main/java/academy/wakanda/wakacop/sessaovotacao/infra/SessaoVotacaoInfraRepository.java
@@ -0,0 +1,23 @@
+package academy.wakanda.wakacop.sessaovotacao.infra;
+
+import academy.wakanda.wakacop.sessaovotacao.application.repository.SessaoVotacaoRepository;
+import academy.wakanda.wakacop.sessaovotacao.domain.SessaoVotacao;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.apache.catalina.Store;
+import org.springframework.stereotype.Repository;
+
+@Log4j2
+@Repository
+@RequiredArgsConstructor
+public class SessaoVotacaoInfraRepository implements SessaoVotacaoRepository {
+    private final SessaoVotacaoSpringDataJPARepository sessaoVotacaoSpringDataJPARepository;
+
+    @Override
+    public SessaoVotacao salva(SessaoVotacao sessaoVotacao) {
+        log.info("[start] SessaoVotacaoInfraRepository - salva");
+        sessaoVotacaoSpringDataJPARepository.save(sessaoVotacao);
+        log.info("[finish] SessaoVotacaoInfraRepository - salva");
+        return sessaoVotacao;
+    }
+}
