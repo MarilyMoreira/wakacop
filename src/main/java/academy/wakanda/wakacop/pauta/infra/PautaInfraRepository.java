fd563b241cfd7034d6f608ac711eaddf1935739a
Marily Moreira <marilysantosmoreira92@gmail.com>
2023-10-31 16:04:06 -0300

    valida abertura sessao votacao

--- src/main/java/academy/wakanda/wakacop/pauta/infra/PautaInfraRepository.java
+++ src/main/java/academy/wakanda/wakacop/pauta/infra/PautaInfraRepository.java
@@ -1,11 +1,14 @@
 package academy.wakanda.wakacop.pauta.infra;
 
+import academy.wakanda.wakacop.handler.APIException;
 import academy.wakanda.wakacop.pauta.application.repository.PautaRepository;
 import academy.wakanda.wakacop.pauta.domain.Pauta;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.catalina.Store;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 @Log4j2
@@ -19,5 +22,14 @@ public class PautaInfraRepository implements PautaRepository {
         pautaSpringDataJPARepository.save(pauta);
         log.info("[finish] PautaInfraRepository - salva");
         return pauta;
+    }
+
+    @Override
+    public Pauta buscaPautaPorId(UUID idPauta) {
+        log.info("[start] PautaInfraRepository - buscaPautaPorId");
+        Pauta pautaPorId = pautaSpringDataJPARepository.findById(idPauta)
+                .orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND,"Pauta não encontrada!"));
+        log.info("[finish] PautaInfraRepository - buscaPautaPorId");
+        return pautaPorId;
     }
 }
